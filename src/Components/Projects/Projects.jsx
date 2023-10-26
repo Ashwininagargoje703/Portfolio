c06cffea8a5044d44767ac68b7cd1e44f6901f65
unknown <ashwininagargoje703@gmail.com>
2023-10-26 14:17:24 +0530

    update

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -22,22 +22,19 @@ const Projects = () => {
     {
       id: 1,
       project_name: "Meshoo Clone",
-      image:
-        "https://i.ibb.co/mcpKr1N/meeshoo.png",
+      image: "https://i.ibb.co/mcpKr1N/meeshoo.png",
       project_desc:
         "Meesho is a online Shopping & Reselling site in India - Buy & Sell best quality Fashion, Electronics, Home & Kitchen products at lowest prices.",
       features:
         "Merchant /User Login, Add a product section, Add to Cart feature",
-      deploy_link:
-        "https://masai-meesho-clone-unit-2-fw13-team-37.netlify.app/",
+      deploy_link: "https://meesho-clone-gold.vercel.app/",
       repo_link: "https://github.com/mangeshniras/meesho_fw_13.git",
       tech_stack: [<FaHtml5 />, <FaCss3 />, <FaJs />],
     },
     {
       id: 2,
       project_name: "TheBowlCompany Clone",
-      image:
-        "https://i.ibb.co/RpHJXfj/TheBowl.png",
+      image: "https://i.ibb.co/RpHJXfj/TheBowl.png",
       project_desc:
         "This is a clone if The Bowl Company Which is about Dishes offered by Swiggys new platform that offers food in a special Bowl.",
       features:
@@ -47,15 +44,47 @@ const Projects = () => {
       repo_link: "https://github.com/MadhanM282/the-bowl-company",
       tech_stack: [<FaHtml5 />, <FaCss3 />, <FaJs />, <FaNodeJs />],
     },
+
+    {
+      id: 6,
+      project_name: "Urboni-Clone",
+      image:
+        "https://raw.githubusercontent.com/Ashwininagargoje703/Urbonic/master/uboric.png",
+      project_desc: "An e-commerce website for users to buy and sell products.",
+      features:
+        "Product details | Add a product section | Add to Cart feature | Sort by price",
+      deploy_link: "https://urbonic.vercel.app/",
+      repo_link: "https://github.com/Ashwininagargoje703/Urbonic",
+      tech_stack: [<FaJs />, <FaHtml5 />, <FaCss3 />],
+    },
+
+    {
+      id: 5,
+      project_name: "Hiring Farm",
+      image:
+        "https://i.ibb.co/XsGpDzw/Whats-App-Image-2023-10-26-at-1-56-40-PM.jpg",
+      project_desc:
+        "Hirning Farm is a comprehensive job platform catering toboth job seekers and employers, offering the ability to postand apply for positions.",
+      features: "Create job Post, Save Jobs, Apply Jobs",
+      deploy_link: "https://hiringfarm.online/",
+      repo_link: "https://github.com/Ashwininagargoje703/Vampire-coders",
+      tech_stack: [
+        <FaJs />,
+        <FaHtml5 />,
+        <FaCss3 />,
+        <FaNodeJs />,
+        <FaReact />,
+      ],
+    },
     {
       id: 3,
       project_name: "Weather-App Google map",
-      image:
-        "https://i.ibb.co/txpKKTc/weather.png",
+      image: "https://i.ibb.co/txpKKTc/weather.png",
       project_desc:
         "A simple App which is used for Seven Day forcasting  weather and location.Also add tempterature and  wind Speed.",
-      features:'This feature displays the current temperature, humidity, wind speed, and other relevant weather conditions for a users current location or a location of their choice.',
-        deploy_link: "https://elegant-jones-1248a2.netlify.app/",
+      features:
+        "This feature displays the current temperature, humidity, wind speed, and other relevant weather conditions for a users current location or a location of their choice.",
+      deploy_link: "https://elegant-jones-1248a2.netlify.app/",
       repo_link: "https://github.com/Ashwininagargoje703/Weather.git",
       tech_stack: [
         // <SiRedux />,
@@ -72,11 +101,11 @@ const Projects = () => {
     {
       id: 4,
       project_name: "McDonald-App",
-      image:
-        "https://i.ibb.co/sRVqwqW/mc-donald.png",
+      image: "https://i.ibb.co/sRVqwqW/mc-donald.png",
       project_desc:
         "A food app here you can search and add foods this page build using html css advace js",
-        features:'The McDonalds app allows users to place their orders, this is web design page ',
+      features:
+        "The McDonalds app allows users to place their orders, this is web design page ",
       deploy_link: "https://epic-poitras-86ac89.netlify.app/",
       repo_link: "https://github.com/Ashwininagargoje703/MCdonals.git",
       tech_stack: [
@@ -87,28 +116,6 @@ const Projects = () => {
         <FaHtml5 />,
         <FaCss3 />,
       ],
-    },
-    // {
-    //     id: 5,
-    //     project_name: 'Weather And Google',
-    //     image: 'https://github.com/Mukesh16407/git_2nd/blob/master/weatherandGoogle_api.png?raw=true',
-    //     project_desc:
-    //         'A simple App which is used for Seven Day forcasting  weather and location.Also add tempterature and  wind Speed.',
-    //     deploy_link: 'https://my-weathermap.netlify.app/',
-    //     repo_link: 'https://github.com/Mukesh16407/weaterandGoolemap',
-    //     tech_stack: [ <FaJs />, <FaHtml5 />, <FaCss3 />],
-    // },
-    {
-      id: 6,
-      project_name: "You-tube Clone",
-      image:
-        "https://i.ibb.co/0MG5tLj/youtube-clone-2.png",
-      project_desc:
-        "Its a app look similar to youtube.also add some functionalty similar to youtube using html css js",
-        features:"show songs list, playlist, and web design",
-      deploy_link: "",
-      repo_link: "https://github.com/Ashwininagargoje703/Youtube-clone.git",
-      tech_stack: [<FaJs />, <FaHtml5 />, <FaCss3 />],
     },
   ];
 
